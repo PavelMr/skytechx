eb72365607609aaa056a73b941bec028bc17f51c
PavelMr <skytech@seznam.cz>
2014-10-25 15:07:47 +0200

    Correction of errors in settings.

--- setting.cpp
+++ setting.cpp
@@ -521,7 +521,7 @@ bool setLoad(QString name, setting_t *set)
   set->map.aster.color = readVal("map.aster.color", MRGB(255, 255, 255), tMap).toUInt();
   set->map.aster.radius = readVal("map.aster.radius", 5, tMap).toInt();
   set->map.aster.plusMag = readVal("map.aster.plusMag", 5, tMap).toFloat();
-  set->map.aster.maxMag = readVal("map.aster.plusMag", 16, tMap).toFloat();
+  set->map.aster.maxMag = readVal("map.aster.magMag", 16, tMap).toFloat();
 
   // milky way
   set->map.milkyWay.bShow = readVal("map.milkyWay.bShow", true, tMap).toBool();
